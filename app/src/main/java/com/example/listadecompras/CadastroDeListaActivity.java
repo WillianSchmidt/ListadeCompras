5fbfef84cded3b65785193a1bd7401781b2ccfa3
MarcusZambrano <marcus_zam@outlook.com>
2019-04-12 02:58:37 -0300

    Segunda tela pronta basic sem teste igual a primeira

---
+++ app/src/main/java/com/example/listadecompras/CadastroDeListaActivity.java
@@ -0,0 +1,14 @@
+package com.example.listadecompras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroDeListaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_de_lista);
+
+    }
+}

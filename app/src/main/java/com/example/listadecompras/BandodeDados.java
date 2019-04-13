34e78c7dba7e12e7b5005e9e521dec1c7ae82dca
MarcusZambrano <marcus_zam@outlook.com>
2019-04-13 04:08:13 -0300

    Banco de Dados pronto SQL Lite porém não testado

---
+++ app/src/main/java/com/example/listadecompras/BandodeDados.java
@@ -0,0 +1,31 @@
+package com.example.listadecompras;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BandodeDados {
+    public class Banco extends SQLiteOpenHelper {
+
+        private static final String NOME_BANCO = "ListaDeCompras";
+        private static final int VERSAO_BANCO = 1;
+
+        public Banco(Context context){
+            super(context, NOME_BANCO, null, VERSAO_BANCO);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE IF NOT EXISTS anotacoes ( " +
+                    "  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT , " +
+                    "  titulo TEXT , " +
+                    "  texto TEXT  ) " );
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+
+}
